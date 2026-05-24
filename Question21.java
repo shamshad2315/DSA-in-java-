69b5659de7e39f47672ce607f79666ae53eb843e
Shamshad Ansari <shamshadansari43984@gmail.com>
2026-05-24 15:52:27 +0530

    Add files via upload

---
+++ Question21.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+ import java.util.*;
+
+public class Question21 {
+  public static void main (String[] jk){
+
+      Scanner sc = new Scanner(System.in);
+      BigInteger a = sc.nextBigInteger();
+      BigInteger b = sc.nextBigInteger();
+
+      System.out.println( a.add(b));
+
+      sc.close();
+  }
+
+}
