780138529d45de492de9c062c9fdca78281efe2f
Shamshad Ansari <shamshadansari43984@gmail.com>
2026-05-31 11:10:15 +0530

    Add files via upload

---
+++ Question30.java
@@ -0,0 +1,20 @@
+//import java.util.*;
+//
+//public class Question30 {
+//    public static void pushAtBottom(int data,Stack<Integer> s)
+//    {
+//        public static void main (String[] args ){
+//             Stack<Integer> s = new Stack<>();
+//             s.push(1);
+//             s.push(2);
+//             s.push(3);
+//
+//             pushAtBottom(data, s);
+//
+//             while(!s.isEmpty()){
+//                 System.out.println(s.peek());
+//                 s.pop();
+//             }
+//        }
+//}
+//}
